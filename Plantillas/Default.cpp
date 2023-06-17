a05a780e8a92e223f20ae460468cf3dd26d4eeea
ReivajRS <javierrisa@gmail.com>
2023-06-17 00:37:48 -0700

    Create Default.cpp

---
+++ Plantillas/Default.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+//Pura gente del coach moy
+using namespace std;
+#define ENDL '\n'
+#define all(s) begin(s), end(s)
+#define rall(n) n.rbegin(), n.rend()
+#define FOR(x, b) for(int x = 0; x <b; x++)
+#define FORE(x, a, b) for(int x = a; x <= b; x++)
+#define FORR(x, a, b) for(int x = a; x >= b; x--)
+#define deb(x)      cerr << #x << " = " << x << '\n';
+#define deb2(x, y)  cerr << #x << " = " << x << ", " << #y << " = " << y << '\n';
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const ll MOD = 1e9+7, INF=1e18;
+
+int main(){_
+    
+    return 0;
+}
