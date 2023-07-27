02a020a1bf9a253341624154d24c655eba3bac91
ReivajRS <javierrisa@gmail.com>
2023-07-26 20:39:49 -0700

    Update Tarjan.cpp

--- Grafos/Tarjan.cpp
+++ Grafos/Tarjan.cpp
@@ -1,27 +1,26 @@
 // Time complexity O(V + E)
-
 typedef vector<int> vi;
-enum { UNVISITED = -1 };
 
-int dfsNumberCounter, numSCC;
+int dfsNumberCounter, numSCC;                           // Variables globales
 vector<vi> AL;
-vi dfs_num, dfs_low, visited;                    // Variables globales
+vi dfs_num, dfs_low, visited;
 stack<int> St;
 
 void tarjanSCC(int u) {
-    dfs_low[u] = dfs_num[u] = dfsNumberCounter;    // dfs_low[u]<=dfs_num[u]
-    dfsNumberCounter++;                            // Incrementa el contador
-    St.push(u);                                    // Para recordar el orden
+    dfs_low[u] = dfs_num[u] = dfsNumberCounter;         // dfs_low[u]<=dfs_num[u]
+    dfsNumberCounter++;                                 // Incrementa el contador
+    St.push(u);                                         // Para recordar el orden
     visited[u] = 1;
+
     for (auto v : AL[u]) {
-        if (dfs_num[v] == UNVISITED)
+        if (dfs_num[v] == -1)                           // No visitado
             tarjanSCC(v);
-        if (visited[v])                              // Condicion de actualizacion
+        if (visited[v])                                 // Condicion de actualizacion
             dfs_low[u] = min(dfs_low[u], dfs_low[v]);
     }
 
-    if (dfs_low[u] == dfs_num[u]) {                // Raiz o inicio de un SCC
-        ++numSCC;                                    // Se aumenta el numero de SCC
+    if (dfs_low[u] == dfs_num[u]) {                     // Raiz o inicio de un SCC
+        ++numSCC;                                       // Se aumenta el numero de SCC
         while (1) {
             int v = St.top(); St.pop(); visited[v] = 0;
             if (u == v) break;
@@ -35,11 +34,12 @@ int main() {
     // Lectura del grafo (Dirigido)
 
     // Ejecucion del algoritmo de Tarjan
-    dfs_num.assign(V, UNVISITED); dfs_low.assign(V, 0); visited.assign(V, 0);
+    dfs_num.assign(V, -1); dfs_low.assign(V, 0); visited.assign(V, 0);
     while (!St.empty()) St.pop();
     dfsNumberCounter = numSCC = 0;
+
     for (int u = 0; u < V; ++u)
-        if (dfs_num[u] == UNVISITED)
+        if (dfs_num[u] == -1)   // No visitado
             tarjanSCC(u);
 
     // Imprime cuantos SCC tiene el grafo
