e9720384e8672ec70863dd23d83ad67cc891af6f
Javier Rivera <124474636+ReivajRS@users.noreply.github.com>
2023-08-26 01:45:53 -0700

    Update Primes.cpp

--- math/Primes.cpp
+++ math/Primes.cpp
@@ -3,57 +3,57 @@ typedef long long ll;
 typedef vector<ll> vll;
 
 ll _sieve_size;
-bitset<10000010> bs;                                // 10^7 es el limite
-vll p;                                              // Vector de primos
+bitset<10000010> bs;
+vll p;
 
-void sieve(ll upperbound) {                         // Rango = [0..upperbound]
-    _sieve_size = upperbound+1;                     // Para incluir el upperbound
-    bs.set();                                       // Asigna 1 en todas las posiciones
-    bs[0] = bs[1] = 0;                              // Excepto indice 0 y 1
+void sieve(ll upperbound) { // Calcula la criba en O(N log(log N))
+    _sieve_size = upperbound+1;
+    bs.set();
+    bs[0] = bs[1] = 0;
     for (ll i = 2; i < _sieve_size; ++i) if (bs[i]){
         // Tacha los multiplos de i a partir de i*i
         for (ll j = i*i; j < _sieve_size; j += i) bs[j] = 0;
-        p.push_back(i);                             // Agrega el primo i a la lista
+        p.push_back(i);
     }
 }
 
-bool isPrime(ll N) {                                // Prueba de primalidad suficientemente buena
-    if (N < _sieve_size) return bs[N];              // O(1) para primos pequenios
+bool isPrime(ll N) {    // Regresa si N es primo. Solo se garantiza su funcionamiento para N <= (ultimo primo en vll p)^2
+    if (N < _sieve_size) return bs[N];
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i)
         if (N%p[i] == 0)
         return false;
-    return true;                                    // Lento si N es un primo grande
-}   // Nota: Solo se garantiza su funcionamiento para N <= (ultimo primo en vll p)^2 
+    return true;
+}
 
-vll primeFactors(ll N) {                            // Pre-condicion, N >= 1
+vll primeFactors(ll N) {    // Regresa un vector con los factores primos de N
     vll factors;
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i)
-        while (N%p[i] == 0) {                       // Se encontro un primo para N
-            N /= p[i];                              // Se remueve de N
+        while (N%p[i] == 0) {
+            N /= p[i];
             factors.push_back(p[i]);
     }
-    if (N != 1) factors.push_back(N);               // La N que queda es un primo
+    if (N != 1) factors.push_back(N);
     return factors;
 }
 
-int numPF(ll N) {
+int numPF(ll N) {   // Regresa el numero de factores primos de N
     int ans = 0;
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i)
         while (N%p[i] == 0) { N /= p[i]; ++ans; }
     return ans + (N != 1);
 }
 
-int numDiffPF(ll N) {
+int numDiffPF(ll N) {   // Regresa el numero de factores primos diferentes de N
     int ans = 0;
     for (int i = 0; i < p.size() && p[i]*p[i] <= N; ++i) {
-        if (N%p[i] == 0) ++ans;                     // Cuenta este factor primo
-        while (N%p[i] == 0) N /= p[i];              // Solo una vez
+        if (N%p[i] == 0) ++ans;
+        while (N%p[i] == 0) N /= p[i];
     }
     if (N != 1) ++ans;
     return ans;
 }
 
-ll sumPF(ll N) {
+ll sumPF(ll N) {    // Regresa la suma de los factores primos de N
     ll ans = 0;
     for (int i = 0; i < p.size() && p[i]*p[i] <= N; ++i)
         while (N%p[i] == 0) { N /= p[i]; ans += p[i]; }
@@ -61,8 +61,8 @@ ll sumPF(ll N) {
     return ans;
 }
 
-int numDiv(ll N) {
-    int ans = 1;                                    // Inicia de ans = 1
+int numDiv(ll N) {  // Regresa el numero de divisores de N
+    int ans = 1;
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i) {
         int power = 0;                              // Cuenta la potencia
         while (N%p[i] == 0) { N /= p[i]; ++power; }
@@ -71,8 +71,8 @@ int numDiv(ll N) {
     return (N != 1) ? 2*ans : ans;                  // Ultimo factor = N^1
 }
 
-ll sumDiv(ll N) {
-    ll ans = 1;                                     // Inicia de ans = 1
+ll sumDiv(ll N) {   // Regresa la suma de los divisores de N
+    ll ans = 1;
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i) {
         ll multiplier = p[i], total = 1;
         while (N%p[i] == 0) {
@@ -86,12 +86,12 @@ ll sumDiv(ll N) {
     return ans;
 }
 
-ll EulerPhi(ll N) {
-    ll ans = N;                                     // Inicia de ans = N
+ll EulerPhi(ll N) { // Regresa cuantos numeros menores a N son coprimos con N
+    ll ans = N;
     for (int i = 0; i < (int)p.size() && p[i]*p[i] <= N; ++i) {
-        if (N%p[i] == 0) ans -= ans/p[i];           // Cuenta los unicos
-        while (N%p[i] == 0) N /= p[i];              // Factor primo
+        if (N%p[i] == 0) ans -= ans/p[i];
+        while (N%p[i] == 0) N /= p[i];
     }
-    if (N != 1) ans -= ans/N;                    	// Ultimo factor
+    if (N != 1) ans -= ans/N;
     return ans;
 }
