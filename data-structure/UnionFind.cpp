2c00b5005bb41d1bcd23b90269e78bedd7298699
Javier Rivera <124474636+ReivajRS@users.noreply.github.com>
2023-08-26 01:12:32 -0700

    Update UnionFind.cpp

--- data-structure/UnionFind.cpp
+++ data-structure/UnionFind.cpp
@@ -1,27 +1,24 @@
-// Union-Find Disjoint Sets escrito en POO, usando las heuristicas de compresion de camino y union por rango
+// Union-Find Disjoint Set usando las heuristicas de compresion de camino y union por rango
 typedef vector<int> vi;
 
-class UnionFind {                                   // Estilo POO
+class UnionFind {
  private:
-    vi p, rank, setSize;                            // vi p es la parte clave
+    vi p, rank, setSize;
     int numSets;
  public:
-    UnionFind(int N) {
-        p.assign(N, 0); for (int i = 0; i < N; ++i) p[i] = i;
-        rank.assign(N, 0);                          // Aceleracion opcional
-        setSize.assign(N, 1);                       // Caracteristica opcional
-        numSets = N;                                // Caracteristica opcional
+    UnionFind(int N) : p(N, 0), rank(N, 0), setSize(N, 1), numSets(N) {
+        for (int i = 0; i < N; ++i) p[i] = i;
     }
     int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
     bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-    int numDisjointSets() { return numSets; }               // Opcional
-    int sizeOfSet(int i) { return setSize[findSet(i)]; }    // Opcional
+    int numDisjointSets() { return numSets; }
+    int sizeOfSet(int i) { return setSize[findSet(i)]; }
     void unionSet(int i, int j) {
-        if (isSameSet(i, j)) return;                // i y j estan en el mismo set
-        int x = findSet(i), y = findSet(j);         // Encuentra los representantes de ambos
+        if (isSameSet(i, j)) return;
+        int x = findSet(i), y = findSet(j);
         if (rank[x] > rank[y]) swap(x, y);          // Para mantener x mas pequenio que y
         p[x] = y;                                   // Set x bajo y
-        if (rank[x] == rank[y]) ++rank[y];          // Acelaracion opcional
+        if (rank[x] == rank[y]) ++rank[y];          // Acelaracion por rango
         setSize[y] += setSize[x];                   // Combina los tamanios de los sets en y
         --numSets;                                  // Una union reduce el numSets
     }
