8e7dbb54ca7d0f9d10c93778a6bdf9e7d7bff09d
Javier Rivera <124474636+ReivajRS@users.noreply.github.com>
2023-08-27 00:30:45 -0700

    Update Dijkstra.cpp

--- graphs/ShortestPath/Dijkstra.cpp
+++ graphs/ShortestPath/Dijkstra.cpp
@@ -1,22 +1,23 @@
-// Time complexity O(E log V)
-
+// Time complexity O(V + E log V)
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
-const int INF=1e9;
+const int INF = 1e9;
 
 vector<vii> adj;
 
-vi dijkstra(int start, int V){
-    priority_queue<ii, vii, greater<ii>> pq;    pq.push({0,start});
-    vi dist(V, INF);    dist[start]=0;
+vi dijkstra(int source, int V){
+    priority_queue<ii, vii, greater<ii>> pq;
+    pq.push({0, source});
+    vi dist(V, INF);
+    dist[source] = 0;
 
     while(!pq.empty()){
         auto [d, u] = pq.top(); pq.pop();
         if(d > dist[u]) continue;
         for(auto &[v, w] : adj[u]){
-            if(dist[u]+w >= dist[v])  continue;
-            dist[v] = dist[u] + w;
+            if(d+w >= dist[v])  continue;
+            dist[v] = d+w;
             pq.push({dist[v], v});
         }
     }
