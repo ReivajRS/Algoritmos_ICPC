e4ac4c40dcc5f64750f11984475f2dfc10160dd2
ReivajRS <javierrisa@gmail.com>
2023-09-01 23:30:53 -0700

    Update SegmentTree.cpp

--- data-structure/SegmentTree/SegmentTree.cpp
+++ data-structure/SegmentTree/SegmentTree.cpp
@@ -1,44 +1,49 @@
-/* Implementacion de segment tree para obtener la suma en un rango, pero es posible usar cualquier
-operacion conmutativa como la multiplicacion, XOR, AND, OR, MIN, MAX, etc.*/
-typedef vector<int> vi;
+/**
+ * Descripcion: arbol de segmentos, bastante poderoso para
+ * realizar consultas de rango y actualizaciones de punto,
+ * se puede utilizar cualquier operacion conmutativa, es decir,
+ * aquella en donde el orden de evaluacion no importe: suma,
+ * multiplicacion, XOR, OR, AND, MIN, MAX, etc.
+ * Tiempo: O(n log n) en construccion y O(log n) por consulta
+ */
 
 class SegmentTree {
-  private:
+ private:
     int n;
     vi arr, st;
 
-    int l(int p) { return p << 1; }       // ir al hijo izquierdo
-    int r(int p) { return (p << 1) + 1; } // ir al hijo derecho
+    int l(int p) { return (p << 1) + 1; }
+    int r(int p) { return (p << 1) + 2; }
 
     void build(int index, int start, int end) {
-        if (start == end) {
-            st[index] = arr[start];
-        } else {
+        if (start == end)
+        st[index] = arr[start];
+        else {
             int mid = (start + end) / 2;
+
             build(l(index), start, mid);
             build(r(index), mid + 1, end);
+
             st[index] = st[l(index)] + st[r(index)];
         }
     }
 
     int query(int index, int start, int end, int i, int j) {
         if (j < start || end < i)
-            return 0; // Si ese rango no nos sirve, retornar un valor que no cambie nada
+            return 0;  // Si ese rango no nos sirve, retornar un valor que no cambie nada
 
         if (i <= start && end <= j)
             return st[index];
 
         int mid = (start + end) / 2;
-        int q1 = query(l(index), start, mid, i, j);
-        int q2 = query(r(index), mid + 1, end, i, j);
 
-        return q1 + q2;
+        return query(l(index), start, mid, i, j) + query(r(index), mid + 1, end, i, j);
     }
 
-    void update(int index, int start, int end, int idx, int val) {
-        if (start == end) {
+  void update(int index, int start, int end, int idx, int val) {
+        if (start == end)
             st[index] = val;
-        } else {
+        else {
             int mid = (start + end) / 2;
             if (start <= idx && idx <= mid)
                 update(l(index), start, mid, idx, val);
@@ -47,17 +52,17 @@ class SegmentTree {
 
             st[index] = st[l(index)] + st[r(index)];
         }
-    }
+  }
 
-  public:
-    SegmentTree(int sz) : n(sz), st(4 * n) {} // Constructor de st sin valores
+ public:
+    SegmentTree(int sz) : n(sz), st(4 * n) {}
 
-    SegmentTree(const vi &initialArr) : SegmentTree((int)initialArr.size()) { // Constructor de st con arreglo inicial
+    SegmentTree(const vi &initialArr) : SegmentTree((int)initialArr.size()) {
         arr = initialArr;
-        build(1, 0, n - 1);
+        build(0, 0, n - 1);
     }
 
-    void update(int i, int val) { update(1, 0, n - 1, i, val); }
+    void update(int i, int val) { update(0, 0, n - 1, i, val); }
 
-    int query(int i, int j) { return query(1, 0, n - 1, i, j); }
+    int query(int i, int j) { return query(0, 0, n - 1, i, j); }  // [i, j]
 };
