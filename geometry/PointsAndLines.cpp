0f992e4251a7dcf7b64fe4af0e0451c64776bf21
ReivajRS <javierrisa@gmail.com>
2023-09-01 23:30:07 -0700

    Update PointsAndLines.cpp

--- geometry/PointsAndLines.cpp
+++ geometry/PointsAndLines.cpp
@@ -67,11 +67,14 @@ struct vec {    // Vector
     vec(double _x, double _y) : x(_x), y(_y) {}
 };
 
-vec toVec(const point &a, const point &b) { return vec(b.x-a.x, b.y-a.y); }         // Convierte 2 puntos en el vector a->b
+// Convierte 2 puntos en el vector a->b
+vec toVec(const point &a, const point &b) { return vec(b.x-a.x, b.y-a.y); }
 
-vec scale(const vec &v, double s) { return vec(v.x*s, v.y*s); }                     // Escala un vector por el valor s
+// Escala un vector por el valor s
+vec scale(const vec &v, double s) { return vec(v.x*s, v.y*s); }
 
-point translate(const point &p, const vec &v) { return point(p.x+v.x, p.y+v.y); }   // Traslada p, de acuerdo a v
+// Traslada p, de acuerdo a v
+point translate(const point &p, const vec &v) { return point(p.x+v.x, p.y+v.y); }
 
 // Convierte un punto y una pendiente en una linea
 void pointSlopeToLine(point p, double m, line &l) {
